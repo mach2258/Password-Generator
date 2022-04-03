5c3c2bcbd292432c52b11b52ce54f7edf2a62b8f
Erich Elste <erichelste@yahoo.com>
2022-04-03 15:51:19 -0400

    finished password gen, changed css values

--- assets/script.js
+++ assets/script.js
@@ -11,65 +11,52 @@ var numbersArray = ["1","2","3","4","5","6","7","8","9","0"]
 
 var symbolsArray = ["!","@","#","$","%","^","&","*"]
 
-var combination = [" "]
+var combination = []
 
 var output = " "
 
 function generatePassword(){
-    console.log("Button clicked")
+    length = 0
+    combination = []
+    output = ""
+    questions()
 
-// 1. promopt user for password criteria
-
-// a. password length 8-128
-    length = prompt("Please pick length of password")
-    console.log(length)
-// b. lowercase,uppercase,numbers, special character prompts
-    lowercase = confirm("Would you like lowercase letters?")
-    console.log(lowercase)
-
-    if(lowercase == true){
-        combination = combination.concat(lowercaseArray)
-    }
-
-    uppercase = confirm("Would you like uppercase letters?")
-    console.log(uppercase)
-
-    if(uppercase == true){
-        combination = combination.concat(uppercaseArray)
-    }
-
-    numbers = confirm("Would you like numbers?")
-    console.log(numbers)
-
-    if(numbers == true){
-        combination = combination.concat(numbersArray)
-    }
-
-    symbols = confirm("Would you like symbols?")
-    console.log(symbols)
-
-    if(symbols == true){
-        combination = combination.concat(symbolsArray)
-    }
-
-        console.log(combination.length)
-
-// 2. validate the input
-// 3. generate password based on criteria
-
-console.log(output)
-
-    if(length > 8 && length < 128){
+    if(length >= 8 && length <= 128){
         for (let index = 0; index < length; index++) {
             output += combination[Math.floor(Math.random() * combination.length)];
             
         }
     }
-    
 
-
-// 4. Display generated password to page
     return output
+}
+function lengthValue(){
+    length = prompt("Please pick length of password (8-128)")
+    console.log(length)
+    if(length < 8 || length > 128 || isNaN(length) === true){
+        lengthValue()
+    }
+}
+
+function questions(){
+    lengthValue()
+
+    lowercase = confirm("Would you like lowercase letters?")
+    if(lowercase == true){
+        combination = combination.concat(lowercaseArray)
+    }
+    uppercase = confirm("Would you like uppercase letters?")
+    if(uppercase == true){
+        combination = combination.concat(uppercaseArray)
+    }
+    numbers = confirm("Would you like numbers?")
+    if(numbers == true){
+        combination = combination.concat(numbersArray)
+    }
+    symbols = confirm("Would you like symbols?")
+    if(symbols == true){
+        combination = combination.concat(symbolsArray)
+    }
 }
 
 // Write password to the #password input
